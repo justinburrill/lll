5c1f193881a780f14ca4c7c64a757ab82e4a4154
Justin Burrill <me@justinburrill.ca>
2024-01-01 21:34:04 -0500

    refactoring

--- super_search.py
+++ super_search.py
@@ -3,11 +3,46 @@
 # 14-12-23
 
 
-from utils import *
 import sys
 from os import listdir, getcwd
 from os.path import isdir, isfile
 
+
+class colour:
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
+
+    def bold(self, s: str) -> str:
+        return self.BOLD + s + self.END
+
+    def blue(self, s: str) -> str:
+        return self.BLUE + s + self.END
+
+
+def bool_input(prompt: str, default: bool = None) -> bool:
+    while True:
+        x = input(prompt).lower()
+        if x.strip() == "":
+            if default is not None:
+                return default
+            else:
+                print("No default provided.")
+                continue
+        elif x in ["y", "yes"]:
+            return True
+        elif x in ["n", "no"]:
+            return False
+        else:
+            print("Invalid input.")
+            continue
 
 def get_children(path: str) -> ([str], [str]):
     path = ensure_trailing_slash(path)
@@ -51,7 +86,6 @@ def ensure_trailing_slash(path: str):
     if not path.endswith("/"):
         path += "/"
     return path
-
 
 def count_descendants(path: str) -> int:
     return 15
